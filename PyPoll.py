c1b64a5774ee332ef5da56f8b30e3b958cb79527
Andrew Nomai <andrewnomai@gmail.com>
2022-07-01 13:15:32 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -17,17 +17,6 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the open() function with the "w" mode we will write data to the file
-
-with open(file_to_save, "w") as txt_file:
-
-# Write three counties to the file.
-
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("-----------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
-
-
 # 1. Initialize a total vote counter.
 
 total_votes = 0
